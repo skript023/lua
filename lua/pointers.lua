b36af460a2cbc4dd5a472fdb96f1d3a202273e1d
skript023 <79650122+skript023@users.noreply.github.com>
2021-03-03 07:10:41 +0700

    Add files via upload

---
+++ lua/pointers.lua
@@ -0,0 +1,87 @@
+require("CPH")
+require("STATS")
+require("instant_heist_array")
+require("local")
+hook = 
+{
+    GET = function (Address)
+        return getAddress(Address)
+    end,
+
+    ptrs = function (Pointers)
+        return readPointer(Pointers)
+    end,
+
+};
+
+
+-----------------Auto Add----------------------------------------------------------------------------------------
+function auto_add()
+  MPX = get.Global(int,1312763)
+  local mp_id = combobox_getItemIndex(Protection_CEComboBox12) + 1;
+	if mp_id==1 then
+    FORM.ADD_LOOP(Protection_CEComboBox1,MP0_Approach,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox2,MP0_TARGET,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox3,MP0_LAST_AP,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox4,MP0_HARD_AP,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox5,MP0_DUGGAN,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox6,MP0_PASS,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox7,MP0_WEP,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox8,MP0_DRIVER,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox9,MP0_HACKER,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox10,MP0_VEH,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox11,MP0_WEPS,1)
+  elseif mp_id==2 then
+    FORM.ADD_LOOP(Protection_CEComboBox1,MP_Cayo_Approach,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox2,MP_Cayo_Target,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox3,MP_Cayo_Diff,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox4,MP_Cayo_Truck,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox5,MP_Cayo_Disrp,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox6,MP_Cayo_Weapon,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox7,NULL_ARRAY,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox8,NULL_ARRAY,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox9,NULL_ARRAY,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox10,NULL_ARRAY,1)
+    sleep(10)
+    FORM.ADD_LOOP(Protection_CEComboBox11,NULL_ARRAY,1)
+  end
+end
+---------------------------------------------------Pointer Struct-------------------------------------------------------
+function Ptr_class()
+  local ptr = readPointer
+  BaseModelPTR = ptr(ptr("ModelHashPTR") + 0x0)
+  ModelHash = ptr(BaseModelPTR + 0x2640)
+  CPlayer     = ptr(ptr("WorldPTR") + 0x8)
+  CPlayerInfo = ptr(CPlayer + 0x10C8)
+  CPlayerPos  = ptr(CPlayer + 0x30)
+  CVehicle    = ptr(CPlayer + 0xD30)
+  CUpgrade    = ptr(CVehicle + 0x48)
+  CHandling   = ptr(CVehicle + 0x938)
+  CWeapon     = ptr(CPlayer + 0x10D8)
+  CInventory  = ptr(CPlayer + 0x10D0)
+  CPInventory = ptr(CInventory + 0x48)
+  CAmmoType   = ptr(CWeapon + 0x20)
+end
+Ptr_class()
+--auto_add()
+
+
